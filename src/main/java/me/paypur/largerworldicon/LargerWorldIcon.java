0f30978022249a5b76854ec7ce0089ee19b028b9
hansenc <6&fFt&X!#eRPN2^oCh^kP&N2R^F2RaCc>
2023-03-03 01:41:57 -0500

    undo deleting main class
    Took 9 minutes

---
+++ src/main/java/me/paypur/largerworldicon/LargerWorldIcon.java
@@ -0,0 +1,11 @@
+package me.paypur.largerworldicon;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("largerworldicon")
+public class LargerWorldIcon
+{
+    public LargerWorldIcon() {
+    }
+}
